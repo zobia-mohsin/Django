a3e6c31a01f8072ccde7931f7bf2c897dc09f217
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-11-29 15:02:12 -0600

    CREATED topics page in class 11/29

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -5,7 +5,10 @@ from . import views #dot means from the same folder
 app_name = "MainApp"
 #1. create url
 #2. create views
-#path is homepage look at index function in views whose name is index
+#path is homepage look at index function in views whose name is index, because location is blank
 urlpatterns = [
     path('',views.index,name='index'),
+    path('topics',views.topics,name='topics') #added a new page, create new view after creating URL
+
 ]
+
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+#create new view after creating URL
+from .models import Topic #. means look in same directory that the views is in
+
 
 # Create your views here.
 def index(request):
     return render(request, "MainApp/index.html") #look at index.html template in views, every view will have an argument request
 #now created folder in mainapp called templates then another folder in it called MainApp
+
+def topics(request):
+    topics = Topic.objects.all().order_by('date_added') #minus for descending, or decensending = FALSE
+    #to return data use dictionary
+    context = {'topics':topics} #THE KEY OF THIS DICTIONARY COMES FROM TEMPLATE, THE VALUE FROM VIEWS
+    return render(request, 'MainApp/topics.html', context) #this returns only the skeletal file, not the data in it
+    #PASS CONTEXT TO RENDER
